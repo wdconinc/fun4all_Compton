0ec65a23c5e10024e53d165b3a2cafed7dbc10b8
Ciprian Gal <ciprian@jlab.org>
2020-05-27 10:01:57 -0400

    not clear if this is needed

--- source/ComptonLinkDef.h
+++ source/ComptonLinkDef.h
@@ -1,4 +1,9 @@
-#if defined(__CINT__) || defined(__CLING__)
+//#if defined(__CINT__) || defined(__CLING__)
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
 
 #pragma link C++ struct comptonTruth_t+;
 #pragma link C++ class vector<comptonTruth_t>+;
